e19d5836b230ef39869b49e163f60a6808f9f5aa
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-08-06 09:32:38 +0100

    Installed and implemented react bootstrap

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -82,7 +82,28 @@ export default class signup extends React.Component {
                 <span>Registration Unsuccesful. Please try again!</span>
               </Alert>)}
 
-            <Form></Form>
+            <Form>
+            <Form.Group controlId="fullName">
+                       <Form.Label class='Label'>fullname</Form.Label>
+                       <Form.Control type="text" onChange={this.handleChange}  placeholder="Enter FullName" />
+                    </Form.Group>
+
+                    <Form.Group controlId="userName">
+                        <Form.Label class='Label'>Username</Form.Label>
+                        <Form.Control type="text"  onChange={this.handleChange}  placeholder="Enter userName" />
+                     </Form.Group>
+
+                    <Form.Group controlId="formGroupEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" block>Submit</Button>
+            </Form>
           </div>
 
         </div>
