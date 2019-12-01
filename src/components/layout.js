f1c4c945bdff2450c6ae8e7259996970c8b9ef8c
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-12-01 18:23:49 +0100

    Improve navbar and logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,26 +3,32 @@ export default function Layout(props) {
   return (
     <div>
       <nav className='nav'>
-        <h1>
-          <a href='/'> Universal Chess Blog</a>
-        </h1>
-        <ul>
-          <li>
-            <a href='/'>Homepage</a>
-          </li>
-          <li>
-            <a href='/signup'>Signup</a>
-          </li>
-          <li>
-            <a href='/login'>Login</a>
-          </li>
-          <li>
-            <a href='/publishpost'>Publishpost</a>
-          </li>
-          <li>
-            <a href='/blog'>Blog</a>
-          </li>
-        </ul>
+        <h4>
+          <a className='logo' href='/'>
+            {' '}
+            Universal Chess Blog
+          </a>
+        </h4>
+
+        <div className='navItems'>
+          <ul>
+            <li>
+              <a href='/'>Homepage</a>
+            </li>
+            <li>
+              <a href='/signup'>Signup</a>
+            </li>
+            <li>
+              <a href='/login'>Login</a>
+            </li>
+            <li>
+              <a href='/publishpost'>Publishpost</a>
+            </li>
+            <li>
+              <a href='/blog'>Blog</a>
+            </li>
+          </ul>
+        </div>
       </nav>
       {props.children}
     </div>
